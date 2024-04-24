4bc6795301d3bb4d5ccbe634b24386976a501afc
smdbs <1289707349@qq.com>
2024-04-23 23:26:18 -0400

    Updated README

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
-## Dataset Used
+## Description
+
+This project aims to build a machine learning model that predicts the type of crime committed in a given area during a given time.
+
+## Run
+
+1. Make sure you have a proper Python version. The author has Python 3.11.7, so any other version may not work as expected.
+
+1. Install the required libraries using `pip install -r requirements.txt`. This might fail if you have a different version of Python.
+
+1. Get the dataset using `python -m src.data.make_dataset`. This will create `data/final/crime_train.csv` and `data/final/crime_test.csv`. If you would like to access the raw data, you can download it directly from [https://catalog.data.gov/dataset/crime-data-from-2020-to-present](https://catalog.data.gov/dataset/crime-data-from-2020-to-present) or change the `is_save` parameter in `src/data/make_dataset.py` to `True`.
+
+1. Train the model using `python -m src.models.train`.
+
+1. Test the model on the test dataset using `python -m src.models.test`.
+
+## Data Source
 
 [https://catalog.data.gov/dataset/crime-data-from-2020-to-present](https://catalog.data.gov/dataset/crime-data-from-2020-to-present)
 
-Downloaded on 2024-4-20.
+The dataset used by this project was downloaded on 4-20-2024.
--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -8,7 +8,7 @@ DATA_PATH = path.join((path.abspath(path.dirname(__file__))), "..", "..", "data"
 # https://catalog.data.gov/dataset/crime-data-from-2020-to-present
 URL = "https://data.lacity.org/api/views/2nrs-mtv8/rows.csv?accessType=DOWNLOAD"
 
-def download_raw_data() -> pd.DataFrame:
+def download_raw_data(is_save: bool = False) -> pd.DataFrame:
     p = path.join(DATA_PATH, "raw", "crime.csv")
     
     if path.exists(p):
@@ -23,15 +23,16 @@ def download_raw_data() -> pd.DataFrame:
     if r.status_code != 200:
         print("Failed to download raw data. Status code:", r.status_code)
         return None
-
-    with open(p, 'wb') as f:
-        f.write(r.content)
+    
+    if is_save:
+        with open(p, 'wb') as f:
+            f.write(r.content)
     
     print("Raw data downloaded.")
     df = pd.read_csv(p)
     return df
 
-def clean_data() -> pd.DataFrame:
+def clean_data(df: pd.DataFrame = None, is_save: bool = False) -> pd.DataFrame:
     cleaned_path = path.join(DATA_PATH, "processed", "crime.csv")
     
     if path.exists(cleaned_path):
@@ -41,14 +42,15 @@ def clean_data() -> pd.DataFrame:
         if i.upper() != "Y":
             return pd.read_csv(cleaned_path)
     
-    p = path.join(DATA_PATH, "raw", "crime.csv")
-    
-    if not path.exists(p):
-        print("Raw data not found. Downloading...")
-        df = download_raw_data()
-    else:
-        print("Raw data already downloaded.")
-        df = pd.read_csv(p)
+    if df is None:
+        p = path.join(DATA_PATH, "raw", "crime.csv")
+        
+        if not path.exists(p):
+            print("Raw data not found. Downloading...")
+            df = download_raw_data()
+        else:
+            print("Raw data already downloaded.")
+            df = pd.read_csv(p)
 
     # drop duplicates
     df = df.drop_duplicates(subset=['DR_NO'])
@@ -56,11 +58,12 @@ def clean_data() -> pd.DataFrame:
     # remove rows missing LAT and LON
     df = df[(df[['LAT', 'LON']] != 0).all(axis=1)]
     
+    if is_save:
+        df.to_csv(cleaned_path, index=False)
+    
     return df
 
-def split_data(df: pd.DataFrame, seed: int = 42, train_size: float = 0.7) -> tuple[pd.DataFrame, pd.DataFrame]:
-    assert df is not None
-    
+def split_data(df: pd.DataFrame = None, seed: int = 42, train_size: float = 0.7, is_save: bool = False) -> tuple[pd.DataFrame, pd.DataFrame]:
     np.random.seed(seed)
     
     train_size = int(len(df) * train_size)
@@ -70,17 +73,20 @@ def split_data(df: pd.DataFrame, seed: int = 42, train_size: float = 0.7) -> tup
     train = df.iloc[shuffle[:train_size]]
     test = df.iloc[shuffle[train_size:]]
     
+    if is_save:
+        train.to_csv(path.join(DATA_PATH, "final", "crime_train.csv"), index=False)
+        test.to_csv(path.join(DATA_PATH, "final", "crime_test.csv"), index=False)
+    
     return train, test
 
 def main():
-    df = download_raw_data()
-    df = clean_data(df)
-
+    df = download_raw_data(is_save=False)
+    print("Raw data created.")
+    
+    df = clean_data(df, is_save=False)
     print("Processed data created.")
 
-    train, test = split_data(df)
-    train.to_csv(path.join(DATA_PATH, "final", "crime_train.csv"), index=False)
-    test.to_csv(path.join(DATA_PATH, "final", "crime_test.csv"), index=False)
+    train, test = split_data(df, is_save=True)
     print("Final train and test data saved.")
 
 if __name__ == "__main__":
